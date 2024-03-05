6c09deaf2db401f6a4e8fd3d0f6b68426586678c
andresantos258 <130004923+andresantos258@users.noreply.github.com>
2024-03-05 00:32:56 +0000

    salvar tudo

---
+++ tests/math.test.js
@@ -0,0 +1,89 @@
+const { continueStatement } = require('@babel/types');
+const math = require('../math');
+var i = 1
+
+// SOMA ***************************************
+for(i=1;i<=100;i++) {
+    var a = 2*i
+    var b = 3+i
+    var c = a + b
+
+    test( 'Resultado da SOMA de ' + a + ' + ' + b + ' é ' + c , () => { 
+        expect(math.add(a, b)).toBe(a+b); 
+        }) 
+}
+for(i=100;i>=0;i--) {
+    var a = 2*i
+    var b = 3+i
+    var c = a + b
+
+    test( 'Resultado da SOMA de ' + a + ' + ' + b + ' é ' + c , () => { 
+        expect(math.add(a, b)).toBe(a+b); 
+        }) 
+}
+
+
+// SUBTRACAO **********************************
+for(i=1;i<=100;i++) {
+    var a = 2*i
+    var b = 3+i
+    var c = a - b
+
+    test( 'Resultado da SUBTRAÇÃO de ' + a + ' - ' + b + ' é ' + c , () => { 
+        expect(math.subtract(a, b)).toBe(a-b); 
+        }) 
+}
+
+for(i=100;i>=0;i--) {
+    var a = 2*i
+    var b = 3+i
+    var c = a - b
+
+    test( 'Resultado da SUBTRAÇÃO de ' + a + ' - ' + b + ' é ' + c , () => { 
+        expect(math.subtract(a, b)).toBe(a-b); 
+        }) 
+}
+
+
+// MULTIPLICACAO *************************
+for(i=1;i<=100;i++) {
+    var a = 2*i
+    var b = 3+i
+    var c = a * b
+
+    test( 'Resultado da MULTIPLICAÇÃO de ' + a + ' * ' + b + ' é ' + c , () => { 
+        expect(math.multiply(a, b)).toBe(a * b); 
+        }) 
+}
+
+for(i=100;i>=0;i--) {
+    var a = 2*i
+    var b = 3+i
+    var c = a * b
+
+    test( 'Resultado da MULTIPLICAÇÃO de ' + a + ' * ' + b + ' é ' + c , () => { 
+        expect(math.multiply(a, b)).toBe(a * b); 
+        }) 
+}
+
+
+//DIVISAO *********************************
+for(i=1;i<=100;i++) {
+    var a = 2*i
+    var b = 3+i
+    var c = a / b
+
+    test( 'Resultado da DIVISÃO de ' + a + ' / ' + b + ' é ' + c , () => { 
+        expect(math.divide(a, b)).toBe(a / b); 
+        }) 
+}
+
+for(i=100;i>=0;i--) {
+    var a = 2*i
+    var b = 3+i
+    var c = a / b
+
+    test( 'Resultado da DIVISÃO de ' + a + ' / ' + b + ' é ' + c , () => { 
+        expect(math.divide(a, b)).toBe(a / b); 
+        }) 
+}
